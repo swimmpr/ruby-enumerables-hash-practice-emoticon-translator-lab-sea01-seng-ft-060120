0113928ac7afe0ffd51b9a7e7a021c0155a031c9
Marshall Raiskin <marshall.raiskin@gmail.com>
2020-05-21 23:10:10 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,10 +5,9 @@ def load_library(file_path)
    new_hash = {} 
 
 emoticons.each  do |key, value| 
-  new_hash = {new_hash[key] = {} 
-   }	   
-   new_hash[key][:english] = value[0] #
-emoticons.each  do |key, value|	   new_hash[key][:japanese] = value[1] 
+  new_hash = { new_hash[key] = {} }	   
+   new_hash[key][:english] = value[0] 
+   new_hash[key][:japanese] = value[1] 
 end
 
 def get_japanese_emoticon
