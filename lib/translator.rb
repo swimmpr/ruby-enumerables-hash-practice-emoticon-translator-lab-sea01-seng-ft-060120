d2c3fab9356212a75e89c9699b0ff7d68274768c
Marshall Raiskin <marshall.raiskin@gmail.com>
2020-05-21 23:05:09 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,7 +5,7 @@ def load_library(file_path)
    new_hash = {} 
 
 emoticons.each  do |key, value| 
-  new_hash = {	   new_hash[key] = {} 
+  new_hash = {new_hash[key] = {} 
    }	   new_hash[key][:english] = value[0] #
 emoticons.each  do |key, value|	   new_hash[key][:japanese] = value[1] 
 end
