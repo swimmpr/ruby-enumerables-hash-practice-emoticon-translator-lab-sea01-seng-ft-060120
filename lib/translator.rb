b8c6dd27e62708dfbbc2ec6373a329b34fed1b9e
Marshall Raiskin <marshall.raiskin@gmail.com>
2020-05-22 00:13:56 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -17,6 +17,14 @@ end
 
 def get_japanese_emoticon(file_path, emoticon)
   # code goes here
+  result = load_library(file_path)
+  meaning = result.keys.find do |key|
+    result[key][:english] == emoticon
+  end
+  if meaning
+    return result [meaning][:japanese]
+  end
+  "sorry, that emoticon was not found"
 end
 
 def get_english_meaning
